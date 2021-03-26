8695810ed042f9ce3c45df1a1dee5dedc2ccb49f
Mollie Goforth <molliegoforth818@gmail.com>
2021-03-26 11:28:53 -0500

    find recipes button working

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,10 @@
       <h1 class="text-center pink--text mb-4">Recipe Assistant</h1>
       <v-container>
         <v-row class="justify-center mt-10">
-          <v-btn color="pink lighten-3" @click="getRandomRecipes">Find recipes</v-btn>
+          <v-btn color="pink lighten-3" @click="getRandomRecipes"
+            >Find recipes</v-btn>
         </v-row>
+        <recipe-results />
       </v-container>
     </v-main>
   </v-app>
@@ -13,13 +15,13 @@
 
 <script>
 import { mapActions } from "vuex";
+import RecipeResults from "./components/RecipeResults.vue";
 export default {
-
   components: {
-
+    RecipeResults,
   },
   methods: {
-    ...mapActions(["getRandomRecipes"])
-  }
+    ...mapActions(["getRandomRecipes"]),
+  },
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -3,9 +3,14 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
+const apiURL = `https://api.spoonacular.com/recipes/random?number=3&apiKey=${process.env.VUE_APP_SPOONACULAR_KEY}`;
+
 export default new Vuex.Store({
   state: {
     searchResults: [],
+    ingredients: [],
+    diets: [],
+    mealType: null,
   },
   mutations: {
     setSearchResults(state, payload){
@@ -17,6 +22,12 @@ export default new Vuex.Store({
       const res = await fetch(getters.recipeURL);
       const data = await res.json();
       commit("setSearchResults", data.recipes);
+    }
+  },
+  getters: {
+    recipeURL({ ingredients, diets, mealType }) {
+      const tags = [...ingredients, ...diets, mealType].join(",").toLowerCase();
+      return `${apiURL}&tags=${tags}`;
     }
   },
   modules: {},
